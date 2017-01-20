ff2e54731cd8d93401557fbb58f276c46521b9d9
pawanpatel1234 <pk39904@gmail.com>
2017-01-20 13:30:51 +0530

    Add files via upload

---
+++ pawan_13014026/app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+
+res.writeHead(200,{'Content-Type':'text/plain'});
+res.write("hey pawan");
+res.end();
+
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('yo pawan , now listening to port 3000');
